2e7e411f10fd3ab7ed293b1fa224de3c67efbb5f
Longajules <judonite60@gmail.com>
2026-09-10 22:01:31 +0200

    ajout de l'exercice 2 et 3 partie 2

--- Project1/exercice2_partie2.cpp
+++ Project1/exercice2_partie2.cpp
@@ -1,11 +1,12 @@
-#include <iostream>
+/*#include <iostream>
 #include <random>
 #include <stdio.h>
 
 int main() {
 	 
 	char caractere[30];
-	int l = 0;
+	int lettre[26] = {0};
+	int nbl = 0;
 	int count = 0;
 	int espace = 0;
 
@@ -14,18 +15,34 @@ int main() {
 		if (caractere[i] == '\n') {
 			break;
 		}
-		l++;
+		nbl++;
 	}
 	
-	for (int b = 0; l > b;b++) {
+	for (int b = 0; nbl > b; b++) {
 		if (caractere[b] == 32) {
 			espace++;
 		}
 		else(count++);
-	}
-	count = count - 1;
 
-	printf("le nombre de caractere est de %d et le nombre d'espace est de %d", p, e);
+		int resultat = 0;
+
+		resultat = caractere[b] - 'a';
+
+		lettre[resultat]++;
+	}
+
+	printf("le nombre de caractere est de %d et le nombre d'espace est de %d\n", count, espace);
+
+	int b = 97;
+
+	for (int i = 0; i < 26; i++) {
+
+		
+
+		(printf("il y a %d %c\n", lettre[i], b));
+		b++;
+	}
 
 return 0;
 }
+*/
---
+++ Project1/exercice3_partie2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <random>
+#include <stdio.h>
+
+int main() {
+
+	int tab[100];
+	int echange = 0;
+
+	for (int i = 0; i < 100; i++) {
+			printf("donnez valeur a triez :");
+			scanf_s("%d", &tab[i]);
+
+			if (tab[i] == -1) {
+
+				break;
+			}
+	}
+	for (int j = 0; j < 100; j++) {
+		for (int i = 0; i < 100; i++) {
+
+			if (tab[i] > tab[i + 1]) {
+
+				echange = tab[i];
+				tab[i] = tab[i + 1];
+				tab[i + 1] = echange;
+			}
+		}
+	}
+
+	return 0;
+}
